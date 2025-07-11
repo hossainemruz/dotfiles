25e1b712afc0dd7990e2bf34bb4dec0870c84c76
Emruz <emruz.hossain@qdrant.com>
2025-07-11 19:56:49 +0600

    Update configuration

---
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      -- add tsx and treesitter
+      vim.list_extend(opts.ensure_installed, {
+        "cmake",
+        "css",
+        "dockerfile",
+        "go",
+        "gomod",
+        "gotmpl",
+        "helm",
+        "scss",
+        "terraform",
+      })
+    end,
+  },
+}
