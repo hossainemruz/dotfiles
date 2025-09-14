faec03199f69f97afd5391ecf68b6149683b6d4c
Emruz Hossain <hossainemruz@gmail.com>
2025-09-14 13:46:38 +0600

    Show current event in next event list

--- dot_config/waybar/scripts/executable_next-events.py
+++ dot_config/waybar/scripts/executable_next-events.py
@@ -2,13 +2,14 @@
 import subprocess
 import datetime
 import json
+import pytz
 
 data = {}
 
 today = datetime.date.today().strftime("%Y-%m-%d")
 
 
-output = subprocess.check_output("gcalcli agenda --tsv", shell=True)
+output = subprocess.check_output("gcalcli agenda --tsv --military", shell=True)
 output = output.decode("utf-8")
 # print(output)
 
@@ -17,15 +18,49 @@ output = output.replace("start_date", "date")
 output = output.replace("start_time", "start")
 output = output.replace("end_time", "end")
 
-lines = output.split("\n")
-first_event = lines[1].split("\t")
-# print(first_event)
 
 # Calcluate widths of the columns
 rows = [line.split("\t") for line in output.strip().split("\n")]
 col_widths = [max(len(str(item)) for item in col) for col in zip(*rows)]
 
+
+def parse_datetime(date_str, time_str):
+    nativ_dt = datetime.datetime.strptime(
+        f"{date_str} {time_str}", "%Y-%m-%d %H:%M")
+    return tz.localize(nativ_dt)
+
+
+# Current date and time
+tz = pytz.timezone('Asia/Dhaka')
+current_time = datetime.datetime.now(tz=tz)
+
+# Find the current or next event
+current_event = None
+next_event = None
+min_time_diff = None
+
+for row in rows[1:]:
+    start_date, start_time, end_date, end_time, *title = row
+    event_start = parse_datetime(start_date, start_time)
+    event_end = parse_datetime(end_date, end_time)
+
+    # Check if the event is currently happening (start_time <= current_time <= end_time)
+    if event_start <= current_time <= event_end:
+        current_event = row
+        break  # Prioritize the current event and stop searching
+
+    # If the event is in the future, check if it's the next upcoming event
+    if event_start > current_time:
+        time_diff = event_start - current_time
+        if min_time_diff is None or time_diff < min_time_diff:
+            min_time_diff = time_diff
+            next_event = row
+
+# print("Current Event: ", current_event)
+# print("Next Event: ", next_event)
+
 # adjust the table with new column width
+# Function to parse date and time into a datetime object
 adjusted_rows = []
 for row in rows:
     ar = [str(item).ljust(width) for item, width in zip(row, col_widths)]
@@ -35,10 +70,14 @@ table = "\n".join(adjusted_rows)
 # print(table)
 
 
-if today in output:
-    data['text'] = f"  {first_event[1]}-{first_event[3]} {first_event[4]}"
+if current_event:
+    data['text'] = f"  {current_event[1]
+                         }-{current_event[3]} {current_event[4]}"
+elif next_event and next_event[0] == today:
+    data['text'] = f"  {next_event[1]
+                         }-{next_event[3]} {next_event[4]}"
 else:
-    data['text'] = " "
+    data['text'] = "  No events today"
 
 data['tooltip'] = table
 
