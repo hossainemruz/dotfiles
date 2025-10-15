06fba5fcccad7ca549715bc99eb7c344f5a7f30b
Emruz Hossain <emruz.hossain@qdrant.com>
2025-10-15 15:32:20 +0600

    Add bashrc

--- dot_config/waybar/scripts/executable_next-events.py
+++ dot_config/waybar/scripts/executable_next-events.py
@@ -1,4 +1,3 @@
-
 import subprocess
 import datetime
 import json
@@ -11,7 +10,8 @@ today = datetime.date.today().strftime("%Y-%m-%d")
 
 
 output = subprocess.check_output(
-    "gcalcli agenda --tsv --military --calendar emruz.hossain@qdrant.com", shell=True)
+    "gcalcli agenda --tsv --military --calendar emruz.hossain@qdrant.com", shell=True
+)
 output = output.decode("utf-8")
 # print(output)
 
@@ -29,13 +29,12 @@ col_widths = [max(len(str(item)) for item in col) for col in zip(*rows)]
 def parse_datetime(date_str, time_str):
     if not time_str:
         time_str = "00:00"
-    nativ_dt = datetime.datetime.strptime(
-        f"{date_str} {time_str}", "%Y-%m-%d %H:%M")
+    nativ_dt = datetime.datetime.strptime(f"{date_str} {time_str}", "%Y-%m-%d %H:%M")
     return tz.localize(nativ_dt)
 
 
 # Current date and time
-tz = pytz.timezone('Asia/Dhaka')
+tz = pytz.timezone("Asia/Dhaka")
 current_time = datetime.datetime.now(tz=tz)
 
 # Find the current or next event
@@ -72,11 +71,9 @@ for index, row in enumerate(rows):
         event_start = parse_datetime(row[0], row[1])
         event_end = parse_datetime(row[2], row[3])
         if event_end < current_time:
-            adjusted_rows.append(f"<s>{ar[0]}   {ar[1]}   {
-                                 ar[3]}   {ar[4]}</s>")
+            adjusted_rows.append(f"<s>{ar[0]}   {ar[1]}   {ar[3]}   {ar[4]}</s>")
         elif event_start <= current_time <= event_end:
-            adjusted_rows.append(f"<b>{ar[0]}   {ar[1]}   {
-                                 ar[3]}   {ar[4]}</b>")
+            adjusted_rows.append(f"<b>{ar[0]}   {ar[1]}   {ar[3]}   {ar[4]}</b>")
         else:
             adjusted_rows.append(f"{ar[0]}   {ar[1]}   {ar[3]}   {ar[4]}")
     else:
@@ -87,15 +84,15 @@ table = "\n".join(adjusted_rows)
 
 
 if current_event:
-    data['text'] = f"  {current_event[1]
-                         }-{current_event[3]} {html.escape(current_event[4])}"
+    data["text"] = (
+        f"  {current_event[1]}-{current_event[3]} {html.escape(current_event[4])}"
+    )
 elif next_event and next_event[0] == today:
-    data['text'] = f"  {next_event[1]
-                         }-{next_event[3]} {html.escape(next_event[4])}"
+    data["text"] = f"  {next_event[1]}-{next_event[3]} {html.escape(next_event[4])}"
 else:
-    data['text'] = "  No events today"
+    data["text"] = "  No events today"
 
-data['tooltip'] = table
+data["tooltip"] = table
 # escape special symbools
 data["tooltip"] = html.escape(data["tooltip"])
 
