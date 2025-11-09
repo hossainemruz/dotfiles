d4e337c36f4d19962e588e2c5968d13a2ef942b6
Emruz Hossain <hossainemruz@gmail.com>
2025-11-09 06:43:30 +0600

    Add highlighting support for templated html

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -5,3 +5,30 @@ vim.opt.relativenumber = false
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "v:lua.require'nvim-treesitter.foldexpr'()"
 vim.opt.foldlevel = 99 -- Start unfolded; adjust as needed
+
+-- Filetype detection for templated YAML files
+vim.filetype.add({
+  extension = {
+    gotmpl = "gotmpl",
+  },
+  -- Apply the extension only for the files that matches this pattern
+  pattern = {
+    [".*/templates/.*%.tpl"] = "helm",
+    [".*/templates/.*%.ya?ml"] = "helm",
+    ["helmfile.*%.ya?ml"] = "helm",
+  },
+})
+
+-- Filetype detection for templated HTML
+vim.filetype.add({
+  extension = {
+    gohtml = "gohtml", -- For files like example.gohtml
+  },
+  -- Apply the extension on files that match this pattern
+  pattern = {
+    [".*/layouts/.*%.html"] = "gohtml",
+  },
+})
+
+-- Associate the custom filetype with the gotmpl parser
+vim.treesitter.language.register("gotmpl", "gohtml")
