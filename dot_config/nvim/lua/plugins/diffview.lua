e153c63eee6c5f7bff193bb191144398e918b9c2
Emruz Hossain <emruz.hossain@qdrant.com>
2026-05-10 05:17:37 +0600

    Add diffview + hardtime nvim plugins

--- dot_config/nvim/lua/plugins/diffview.lua
+++ dot_config/nvim/lua/plugins/diffview.lua
@@ -1,3 +1,27 @@
 return {
   "sindrets/diffview.nvim",
+  cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles" },
+  opts = {
+    enhanced_diff_hl = true, -- See ':h diffview-config-enhanced_diff_hl'
+    view = {
+      default = {
+        winbar_info = true, -- See ':h diffview-config-view.x.winbar_info'
+      },
+    },
+  },
+  keys = {
+    { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Diffview Open" },
+    { "<leader>gF", "<cmd>DiffviewFileHistory %<cr>", desc = "Diffview File History" },
+    {
+      "<leader>gv",
+      function()
+        if next(require("diffview.lib").views) == nil then
+          vim.cmd("DiffviewOpen")
+        else
+          vim.cmd("DiffviewClose")
+        end
+      end,
+      desc = "Toggle Diffview",
+    },
+  },
 }
